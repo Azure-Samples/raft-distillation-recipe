c968db43da9cf18270c2fca4b3fce9117d0223ca
Cedric Vidal <cedric@vidal.biz>
2024-07-19 20:22:14 +0000

    Idempotent setup script

--- setup_raft.sh
+++ setup_raft.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-git clone --no-checkout --depth=1 --filter=tree:0 https://github.com/cedricvidal/gorilla.git --branch upstream-merge-prep --single-branch
-cd gorilla/
+set -e
+
+RAFT_DIR=${RAFT_DIR:-${PWD}/.gorilla}
+echo "Setup Gorilla RAFT in ${RAFT_DIR}"
+if [ ! -d "${RAFT_DIR}" ]; then
+    echo "Checking out the Gorilla RAFT repo"
+    git clone --no-checkout --depth=1 --filter=tree:0 https://github.com/cedricvidal/gorilla.git --branch upstream-merge-prep --single-branch ${RAFT_DIR}
+fi
+cd ${RAFT_DIR}
 git sparse-checkout set --no-cone raft
 git checkout
