a9d13aee19b51bfe8f4784df0324ded81fc3ad2d
Cedric Vidal <cedric@vidal.biz>
2024-08-02 17:52:09 +0000

    Moved report project scope logic to utils.py

--- utils.py
+++ utils.py
@@ -10,3 +10,23 @@ def get_pdf_image(doc_path):
         img.background_color = 'white'
         img.alpha_channel = 'remove'
     return img
+
+def get_reporting_project_scope():
+    from os import getenv
+    subscription_id = getenv("REPORT_SUB_ID")
+    resource_group_name = getenv("REPORT_GROUP")
+    project_name = getenv("REPORT_PROJECT_NAME")
+    report_vars = [subscription_id, resource_group_name, project_name]
+    project_scope_report = None
+    if any(report_vars):
+        print("Configuring AI Studio Reporting")
+        if any(not var for var in report_vars):
+            raise ValueError("In order to use AI Studio reporting, please set all REPORT_SUB_ID, REPORT_GROUP, and REPORT_PROJECT_NAME environment variables.")
+        project_scope_report = {
+            "subscription_id": subscription_id,
+            "resource_group_name": resource_group_name,
+            "project_name": project_name,
+        }
+    else:
+        print("Skipping AI Studio Reporting")
+    return project_scope_report
