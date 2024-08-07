5ac2d62c44e4860fe6fadc487cf90e366a31c01a
Cedric Vidal <cedric@vidal.biz>
2024-08-07 07:27:55 +0000

    run_all.sh takes parameters

--- run_all.sh
+++ run_all.sh
@@ -6,5 +6,5 @@ mkdir -p work
 rm -rf work/notebooks
 mkdir -p work/notebooks
 for i in *.ipynb; do
-    papermill --request-save-on-cell-execute --autosave-cell-every 10 $i work/notebooks/$i
+    papermill --request-save-on-cell-execute --autosave-cell-every 10 $i work/notebooks/$i ${*}
 done
