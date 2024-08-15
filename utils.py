f4d909752965316ad48348d7acf7d688dfd6aff4
Cedric Vidal <cedric@vidal.biz>
2024-08-15 22:56:23 +0000

    Black formating

--- utils.py
+++ utils.py
@@ -2,28 +2,32 @@ def get_pdf_image(doc_path):
     """Converts a PDF document to an image."""
     from wand.image import Image as WImage
     from pathlib import Path
+
     img = None
     if Path(doc_path).exists() and Path(doc_path).is_file():
         img = WImage(filename=doc_path)
 
         # make background of img white
-        img.format = 'png'
-        img.background_color = 'white'
-        img.alpha_channel = 'remove'
+        img.format = "png"
+        img.background_color = "white"
+        img.alpha_channel = "remove"
     return img
+
 
 def update_state(key, value):
     """Update the .env.state state file with the key and value."""
     from pathlib import Path
     from dotenv import dotenv_values
+
     data = {}
-    state_filename = '.env.state'
+    state_filename = ".env.state"
     if Path(state_filename).exists() and Path(state_filename).is_file():
         data = dotenv_values(state_filename)
     data[key] = value
-    with open(state_filename, 'w') as f:
+    with open(state_filename, "w") as f:
         for k, v in data.items():
             f.write(f"{k}={v}\n")
+
 
 def wait_for_model(client, model_name):
     """Wait for the model to be available, typically waiting for a finetuning job to complete."""
@@ -39,8 +43,10 @@ def wait_for_model(client, model_name):
             attempts += 1
             time.sleep(30)
 
+
 def file_sha256(filename):
     """Returns the SHA256 hash of a file."""
     import hashlib
-    with open(filename, 'rb', buffering=0) as f:
-        return hashlib.file_digest(f, 'sha256').hexdigest()
+
+    with open(filename, "rb", buffering=0) as f:
+        return hashlib.file_digest(f, "sha256").hexdigest()
