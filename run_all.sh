320c95fb28913349bfc4b18d1b487386f66a5001
Cedric Vidal <cedric@vidal.biz>
2024-08-26 17:24:24 +0000

    Removed infra notebooks from run_all script

--- run_all.sh
+++ run_all.sh
@@ -5,6 +5,6 @@ set -e
 mkdir -p work
 rm -rf work/notebooks
 mkdir -p work/notebooks
-for i in $(find . -type f -name "*.ipynb" -maxdepth 1 | sort); do
-    echo papermill --request-save-on-cell-execute --autosave-cell-every 10 $i work/notebooks/$i ${*}
+for i in *.ipynb; do
+    papermill --request-save-on-cell-execute --autosave-cell-every 10 $i work/notebooks/$i ${*}
 done
