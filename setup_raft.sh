415999cb1e5ef48e3557199b4f63b03c0d9bc9e8
Cedric Vidal <cedric@vidal.biz>
2024-08-27 17:48:36 +0000

    Using different gorilla branch

--- setup_raft.sh
+++ setup_raft.sh
@@ -6,7 +6,7 @@ RAFT_DIR=${RAFT_DIR:-${PWD}/.gorilla}
 echo "Setup Gorilla RAFT in ${RAFT_DIR}"
 if [ ! -d "${RAFT_DIR}" ]; then
     echo "Checking out the Gorilla RAFT repo"
-    git clone --no-checkout --depth=1 --filter=tree:0 https://github.com/cedricvidal/gorilla.git --branch upstream-merge-prep --single-branch ${RAFT_DIR}
+    git clone --no-checkout --depth=1 --filter=tree:0 https://github.com/cedricvidal/gorilla.git --branch raft-distillation-recipe --single-branch ${RAFT_DIR}
 fi
 cd ${RAFT_DIR}
 git sparse-checkout set --no-cone raft
