6e791de1a61012672463b69cd0b589dbdd8bc7e9
Cedric Vidal <cedric@vidal.biz>
2024-09-26 02:27:19 +0000

    Reading roles from ai.yaml

--- infra/azd/hooks/select_models.py
+++ infra/azd/hooks/select_models.py
@@ -13,11 +13,16 @@ def read_ai_config():
         aiConfig = yaml.safe_load(aiConfigFile)
     return aiConfig
 
+def get_roles(aiConfig):
+    deployments=aiConfig['deployments'] if 'deployments' in aiConfig else []
+    roles = set([role for d in deployments for role in d['roles']])
+    return roles
+
 @cache
 def get_deployment_names(role='teacher'):
     aiConfig = read_ai_config()
     deployments=aiConfig['deployments'] if 'deployments' in aiConfig else []
-    
+
     teacherDeployments = filter(lambda d: role in d['roles'], deployments)
     deploymentNames = map(lambda d: d['name'], teacherDeployments)
     return list(deploymentNames)
@@ -28,7 +33,7 @@ def first(array):
 def select_model(role, names):
     import survey
     index = survey.routines.select(f"Pick a {role} deployment name: ", options = names)
-    deployment=teacher_deployment_names[index]
+    deployment=names[index]
     return deployment
 
 def yad(decorators):
@@ -38,30 +43,37 @@ def yad(decorators):
         return f
     return decorator
 
+def role_env_var_name(role):
+    return f'{role.upper()}_DEPLOYMENT_NAME'
+
 if __name__ == '__main__':
+
+    ai_config = read_ai_config()
+    roles = get_roles(ai_config)
 
     teacher_deployment_names = get_deployment_names(role='teacher')
     baseline_deployment_names = get_deployment_names(role='baseline')
 
     deployment_names = []
 
-    roles = ['teacher', 'baseline']
     options = [
         click.option(f'--{role}-deployment',
             type=click.Choice(get_deployment_names(role=role)),
-            default=os.getenv(f'{role.upper()}_DEPLOYMENT_NAME'),
+            default=os.getenv(role_env_var_name(role)),
             help=f'The name of the {role} deployment to select.'
             ) for role in roles]
 
     @click.command()
     @yad(options)
-    def select_models(baseline_deployment, teacher_deployment):
-        if not teacher_deployment:
-            teacher_deployment = select_model('teacher', teacher_deployment_names)
-        if not baseline_deployment:
-            baseline_deployment = select_model('baseline', baseline_deployment_names)
-
-        click.echo(f'Teacher deployment {teacher_deployment}')
-        click.echo(f'Baseline deployment {baseline_deployment}')
+    def select_models(**kwargs):
+        for arg_name, arg_value in kwargs.items():
+            if not arg_value:
+                role = arg_name.replace('_deployment', '')
+                names = get_deployment_names(role=role)
+                if len(names) == 1:
+                    arg_value = names[0]
+                else:
+                    arg_value = select_model(role, names)
+            click.echo(f'{role_env_var_name(role)}={arg_value}')
 
     select_models()
