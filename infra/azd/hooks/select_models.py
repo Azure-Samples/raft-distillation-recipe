bc68e506ddb803e93362ca73b419313d5dd652a4
Cedric Vidal <cedric@vidal.biz>
2024-09-26 01:32:33 +0000

    Selection input for models using survey

--- infra/azd/hooks/select_models.py
+++ infra/azd/hooks/select_models.py
@@ -18,25 +18,35 @@ def get_deployment_names(role='teacher'):
 def first(array):
     return next(iter(array), None)
 
+def select_model(role, names):
+    import survey
+    index = survey.routines.select(f"Pick a {role} deployment name: ", options = names)
+    deployment=teacher_deployment_names[index]
+    return deployment
+
 if __name__ == '__main__':
 
-    teacher_deployment_names = get_deployment_names('teacher')
-    baseline_deployment_names = get_deployment_names('baseline')
+    teacher_deployment_names = get_deployment_names(role='teacher')
+    baseline_deployment_names = get_deployment_names(role='baseline')
 
     @click.command()
     @click.option('--teacher-deployment',
-        prompt='Teacher deployment name',
         type=click.Choice(teacher_deployment_names),
-        default=os.getenv('TEACHER_DEPLOYMENT_NAME', first(teacher_deployment_names)),
+        default=os.getenv('TEACHER_DEPLOYMENT_NAME'),
         help='The name of the teacher deployment to select.'
         )
     @click.option('--baseline-deployment',
-        prompt='Baseline deployment name',
         type=click.Choice(baseline_deployment_names),
         default=os.getenv('BASELINE_DEPLOYMENT_NAME', first(baseline_deployment_names)),
         help='The name of the baseline deployment to select.'
         )
     def select_models(teacher_deployment, baseline_deployment):
+        if not teacher_deployment:
+            teacher_deployment = select_model('teacher', teacher_deployment_names)
+        if not baseline_deployment:
+            baseline_deployment = select_model('baseline', baseline_deployment_names)
+
         click.echo(f'Teacher deployment {teacher_deployment}')
+        click.echo(f'Baseline deployment {baseline_deployment}')
 
     select_models()
