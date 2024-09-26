04f147e15fb87b1b3b0adffaf85e54a59569aaa6
Cedric Vidal <cedricvidal@microsoft.com>
2024-09-26 23:20:16 +0000

    azd env values loaded after .env

--- infra/scripts/select_models.py
+++ infra/scripts/select_models.py
@@ -10,6 +10,8 @@ import subprocess
 
 def azd_env_get_values():
     result = subprocess.run(['azd', 'env', 'get-values'], capture_output=True, text=True)
+    if result.returncode:
+        raise Exception("Failed to get azd environment values because of: " + result.stdout)
     return result.stdout
 
 def load_azd_env():
@@ -18,12 +20,12 @@ def load_azd_env():
     config = StringIO(env_values)
     load_dotenv(stream=config)
 
+load_azd_env()
 load_dotenv(".env")
 load_dotenv(".env.state")
-load_azd_env()
 
 def read_ai_config():
-    dir=cwd = os.path.dirname(os.path.realpath(__file__))
+    dir = os.path.dirname(os.path.realpath(__file__))
     path=Path(dir, "../azd/ai.yaml")
     with open(path, 'r') as aiConfigFile:
         aiConfig = yaml.safe_load(aiConfigFile)
