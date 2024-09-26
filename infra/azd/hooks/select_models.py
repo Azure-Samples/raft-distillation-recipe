22701695824e770f85995986c176390f4a951655
Cedric Vidal <cedric@vidal.biz>
2024-09-26 05:20:18 +0000

    Simplifying the code

--- infra/azd/hooks/select_models.py
+++ infra/azd/hooks/select_models.py
@@ -3,9 +3,7 @@ import os
 from rich.prompt import Prompt
 import yaml
 from pathlib import Path
-from functools import cache
 
-@cache
 def read_ai_config():
     dir=cwd = os.path.dirname(os.path.realpath(__file__))
     path=Path(dir, "../ai.yaml")
@@ -18,10 +16,8 @@ def get_roles(aiConfig):
     roles = set([role for d in deployments for role in d['roles']])
     return roles
 
-@cache
-def get_deployment_names(role='teacher'):
-    aiConfig = read_ai_config()
-    deployments=aiConfig['deployments'] if 'deployments' in aiConfig else []
+def get_deployment_names(ai_config, role='teacher'):
+    deployments=ai_config['deployments'] if 'deployments' in ai_config else []
 
     teacherDeployments = filter(lambda d: role in d['roles'], deployments)
     deploymentNames = map(lambda d: d['name'], teacherDeployments)
@@ -43,6 +39,18 @@ def yad(decorators):
         return f
     return decorator
 
+def role_option(ai_config, role):
+    return click.option(f'--{role}-deployment',
+        type=click.Choice(get_deployment_names(ai_config=ai_config, role=role)),
+        default=os.getenv(role_env_var_name(role)),
+        help=f'The name of the {role} deployment to select.'
+        )
+
+def role_options(ai_config):
+    roles = get_roles(ai_config)
+    options = [role_option(ai_config, role) for role in roles]
+    return yad(options)
+
 def role_env_var_name(role):
     return f'{role.upper()}_DEPLOYMENT_NAME'
 
@@ -51,25 +59,16 @@ if __name__ == '__main__':
     ai_config = read_ai_config()
     roles = get_roles(ai_config)
 
-    teacher_deployment_names = get_deployment_names(role='teacher')
-    baseline_deployment_names = get_deployment_names(role='baseline')
-
-    deployment_names = []
-
-    options = [
-        click.option(f'--{role}-deployment',
-            type=click.Choice(get_deployment_names(role=role)),
-            default=os.getenv(role_env_var_name(role)),
-            help=f'The name of the {role} deployment to select.'
-            ) for role in roles]
+    options = [role_option(ai_config, role) for role in roles]
 
     @click.command()
-    @yad(options)
+#    @yad(options)
+    @role_options(ai_config)
     def select_models(**kwargs):
         for arg_name, arg_value in kwargs.items():
             if not arg_value:
                 role = arg_name.replace('_deployment', '')
-                names = get_deployment_names(role=role)
+                names = get_deployment_names(ai_config=ai_config, role=role)
                 if len(names) == 1:
                     arg_value = names[0]
                 else:
