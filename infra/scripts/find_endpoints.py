413a89629470ea41cbdfb2a46e792d1f3f84fc08
Cedric Vidal <cedric@vidal.biz>
2024-10-05 05:15:49 +0000

    fine_endpoints now respects role assignments

--- infra/scripts/find_endpoints.py
+++ infra/scripts/find_endpoints.py
@@ -58,49 +58,66 @@ def read_env_role(role):
         "model_name": os.getenv(role_model_env_var_name(role)),
     }
 
-@click.command()
-def find_endpoints():
+def get_az_ad_signed_in_user():
+    result = subprocess.run(['az', 'ad', 'signed-in-user', 'show', "--query", "id"], capture_output=True, text=True)
+    if result.returncode:
+        raise Exception("Failed to run command: " + result.stdout)
+    return json.loads(result.stdout)
 
-    config = read_ai_config()
-    roles = get_roles(config.data)
+def get_role_assignments(user_id, scope, role):
+    result = subprocess.run([
+        'az', 'role', 'assignment', 'list', 
+        '--assignee', user_id, 
+        '--out', 'json',
+        '--scope', scope,
+        '--include-inherited',
+        '--role', role
+        ], capture_output=True, text=True)
+    if result.returncode:
+        raise Exception("Failed to run command: " + result.stdout)
+    return json.loads(result.stdout)
 
-    model_names = []
-    for role_name in roles:
-        click.echo(f"Role {role_name}")
-        role = read_env_role(role_name)
-        model_names.append(role['model_name'])
+def has_role_assignment(user_id, scope, role):
+    assignments = get_role_assignments(user_id, scope, role)
+    return len(assignments) > 0
 
-    click.echo(f"Searching for endpoints")
+openai_user_role_id = '5e0bd9bd-7b93-4f28-af87-19fc36ad61bd' # Cognitive Services OpenAI User
+
+def do_scan_azure():
+    click.echo(f"Scanning Azure for OpenAI endpoints")
     accounts = list_cognitive_services_accounts()
     endpoints = {}
+    signed_in_user_id = get_az_ad_signed_in_user()
     for account_digest in accounts:
-        click.echo(f"Found account: {account_digest['id']}")
-        account = get_cognitive_services_account(account_digest['id'])
-        oai_endpoint = get_oai_endpoint(account)
-        #click.echo(f"OpenAI endpoint: {oai_endpoint}")
-        deployments = get_deployments(account_digest['id'])
-        for deployment in deployments:
-            deployment_name = deployment['name']
-            deployment_properties = deployment['properties']
-            model = deployment_properties['model']
-            model_name = model['name']
-            rateLimits = get_deployment_rate_limits(deployment)
-            sku = deployment['sku']
-            sku_capacity = sku['capacity']
-            sku_name = sku['name']
+        account_id = account_digest['id']
+        if has_role_assignment(signed_in_user_id, account_id, openai_user_role_id):
+            click.echo(f"OpenAI resource with role assignment: {account_id}")
+            account = get_cognitive_services_account(account_id)
+            oai_endpoint = get_oai_endpoint(account)
+            #click.echo(f"OpenAI endpoint: {oai_endpoint}")
+            deployments = get_deployments(account_id)
+            for deployment in deployments:
+                deployment_name = deployment['name']
+                deployment_properties = deployment['properties']
+                model = deployment_properties['model']
+                model_name = model['name']
+                rateLimits = get_deployment_rate_limits(deployment)
+                sku = deployment['sku']
+                sku_capacity = sku['capacity']
+                sku_name = sku['name']
 
-#            if model_name in model_names:
-            if not model_name in endpoints.keys():
-                endpoints[model_name] = []
-            endpoints[model_name].append({
-                "deployment_name": deployment_name,
-                "sku_capacity": sku_capacity,
-                "sku_name": sku_name,
-                "endpoint": oai_endpoint
-            })
-            click.echo(f"Adding OpenAI endpoint: {model_name} {oai_endpoint} ({sku_capacity} {sku_name})")
-#            else:
-#                click.echo(f"Skipping OpenAI endpoint: {model_name} {oai_endpoint} ({sku_capacity} {sku_name})")
+    #            if model_name in model_names:
+                if not model_name in endpoints.keys():
+                    endpoints[model_name] = []
+                endpoints[model_name].append({
+                    "deployment_name": deployment_name,
+                    "sku_capacity": sku_capacity,
+                    "sku_name": sku_name,
+                    "endpoint": oai_endpoint
+                })
+                click.echo(f"Adding OpenAI endpoint: {model_name} {oai_endpoint} ({sku_capacity} {sku_name})")
+    #            else:
+    #                click.echo(f"Skipping OpenAI endpoint: {model_name} {oai_endpoint} ({sku_capacity} {sku_name})")
 
     model_list = []
     litellm_config = {
@@ -119,12 +136,48 @@ def find_endpoints():
                 "litellm_params": {
                     "model": f"azure/{deployment_name}",
                     "api_base": api_base,
-                    "tpm": tpm
+                    "tpm": tpm,
+                    "tenant_id": "HACK"
                 }
             })
-    click.echo(f"Writing litellm_config.yaml")
-    with open('litellm_config.yaml', 'w') as f:
-        yaml.dump(litellm_config, f)
+
+    return litellm_config
+
+def export_proxy_endpoints():
+    config = read_ai_config()
+    roles = get_roles(config.data)
+
+    model_names = []
+    env_values = []
+    for role_name in roles:
+        click.echo(f"Exporting env vars for role {role_name}")
+        role = read_env_role(role_name)
+        model_name = role['model_name']
+        model_names.append(model_name)
+        env_values.append((f"{role_name.upper()}_OPENAI_BASE_URL", "http://localhost:4000/"))
+        env_values.append((f"{role_name.upper()}_OPENAI_API_KEY", "DUMMY"))
+        env_values.append((f"{role_name.upper()}_OPENAI_DEPLOYMENT", model_name))
+
+    with open('.env.scan', 'w') as f:
+        for name, value in env_values:
+            f.write(f'{name}={value}\n')
+    #        azd_set_env(name, value)
+
+
+@click.command()
+@click.option('--scan-azure/--no-scan-azure', default=True, help='Whether to scan Azure searching for OpenAI endpoints.')
+@click.option('--set-azd-env/--no-set-azd-env', default=True, help='Set the selected deployment names as azd environment variables.')
+def find_endpoints(scan_azure, set_azd_env):
+
+    if scan_azure:
+        litellm_config = do_scan_azure()
+
+        click.echo(f"Writing litellm_config.yaml")
+        with open('litellm_config.yaml', 'w') as f:
+            yaml.dump(litellm_config, f)
+
+    if set_azd_env:
+        export_proxy_endpoints()
 
 if __name__ == '__main__':
     find_endpoints()
